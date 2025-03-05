70e8f42826620a19ffac1e3c3524d050a878ab12
LucivaldoDev <lucivaldo.jr@dobemtecnologia.com>
2025-03-04 21:41:43 -0300

    Add files via upload

---
+++ aula_01.py
@@ -0,0 +1,24 @@
+# O interpretador do python faz a leitura mas não imprime na tela mas guarda na memória.
+
+"""
+DocString
+Utilizando 3 aspas duplas, triplas. 
+
+Usado para comentar ou 
+documentar bem direitinho seu codigo
+
+"""
+
+'''
+Também é uma DocString
+Ou utilizando 3 aspas simples, triplas.
+Que servirão do mesmo jeito.
+
+Pode ficar tudo em uma linha ou em multiplas linhas.
+'''
+
+
+# Permite criar um comentário de linha única
+print(1987) # pode ficar depois da função
+# ou abaixo da função.
+print(2025)
