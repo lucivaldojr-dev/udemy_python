b56a774adf847db202ce32d806eb8da569be2f5f
LucivaldoDev <lucivaldo.jr@dobemtecnologia.com>
2025-03-05 11:22:55 -0300

    aula 3 em andamento

--- aula_02.py
+++ aula_02.py
@@ -1,1 +1,4 @@
-print("ano", 1987)
+# para argumentos não nomeados (nesse caso os números) com o argumento sep=''(separador) para mudar literalemnte o separados
+print(12, 34, sep="-") # dentro das aspas (que podem ser duplas ou simples)
+print(56, 78, sep="") # aqui no caso não muda nada, pois permanece o que já é por padrão
+print(3, 9, sep="-")
---
+++ aula_03.py
@@ -0,0 +1,15 @@
+"""
+Python = Linguagem de Programação
+Tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings = são textos, que por sua vez ficam dentro das aspas
+"""
+
+# Aspas simples
+ 
+# Aspas duplas
+
+# Escape
+
+# 
+ 
